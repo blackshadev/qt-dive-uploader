376383d3d467d6a8b212642f0e9c7680784b1dc7
Vincent Hagen <vinnie@script4web.nl>
2018-03-07 19:19:58 +0100

    working on requests and access token

--- littledivelog/littledivelog.cpp
+++ littledivelog/littledivelog.cpp
@@ -41,6 +41,77 @@ void LittleDiveLog::login(QString email, QString password)
     req->send();
 }
 
+void LittleDiveLog::get_access_token(std::function<void()> callback)
+{
+    raw_request(
+        RequestMethod::GET,
+        "access-token",
+        TokenType::REFRESH,
+        NULL,
+        [=](JsonResponse resp) {
+            auto obj = resp.data.object();
+            if(resp.statuscode == 401) {
+                emit error("Invalid refresh token");
+                m_access_token.clear();
+                m_refresh_token.clear();
+                emit loggedOut();
+            } else if(!obj.contains("jwt")) {
+                throw std::runtime_error("Expected webservice to return jwt");
+            } else {
+                m_access_token = obj["jwt"].toString();
+                callback();
+            }
+
+        }
+    );
+}
+
+void LittleDiveLog::raw_request(RequestMethod method, QString path, TokenType tokenType, QJsonObject *data, std::function<void(JsonResponse)> callback)
+{
+    JsonRequest* req = new JsonRequest();
+    req->url = "https://dive.littledev.nl/api/" + path;
+    req->method = method;
+
+    switch(tokenType) {
+        case TokenType::ACCESS:
+            req->jwt = m_access_token;
+        break;
+        case TokenType::REFRESH:
+            req->jwt = m_refresh_token;
+        break;
+    }
+
+    if(data != NULL) {
+        req->data.setObject(*data);
+    }
+
+    connect(req, &JsonRequest::complete, this, [=](JsonResponse resp) {
+        if(resp.parseError.error != QJsonParseError::NoError) {
+            emit error(resp.parseError.errorString());
+        } else {
+            callback(resp);
+        }
+        delete req;
+    });
+
+    req->send();
+}
+
+void LittleDiveLog::request(RequestMethod method, QString path, QJsonObject *data, std::function<void(JsonResponse)> callback)
+{
+    if(!isLoggedIn()) {
+        emit error("Cannot execute request. Not yet logged in.");
+        return;
+    }
+    if(m_access_token.isEmpty()) {
+        get_access_token(
+            [=]() {
+                request(method, path, data, callback);
+            }
+        );
+    }
+}
+
 void LittleDiveLog::logout() {
     if(!isLoggedIn()) {
         emit error("Cannot logout, while not yet logged in.");
--- littledivelog/littledivelog.h
+++ littledivelog/littledivelog.h
@@ -3,6 +3,14 @@
 
 #include <QObject>
 #include <QtNetwork>
+#include "../jsonrequest.h"
+#include <functional>
+
+enum TokenType {
+    NONE,
+    REFRESH,
+    ACCESS
+};
 
 class LittleDiveLog : public QObject
 {
@@ -19,6 +27,9 @@ signals:
     void loggedOut();
 public slots:
 protected:
+    void get_access_token(std::function<void()> callback);
+    void request(RequestMethod method, QString path, QJsonObject* data, std::function<void(JsonResponse)> callback );
+    void raw_request(RequestMethod method, QString path, TokenType tokenType, QJsonObject* data, std::function<void(JsonResponse)> callback );
     QString m_refresh_token;
     QString m_access_token;
 };
