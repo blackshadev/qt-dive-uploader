3912e2c512459fb6115cab4eae950de6599e7a31
Vincent Hagen <vinnie@script4web.nl>
2018-05-20 09:12:26 +0200

    signals does not work on selectionwriter

--- libdivecomputer/writer/selectionwriter.cpp
+++ libdivecomputer/writer/selectionwriter.cpp
@@ -3,6 +3,21 @@
 SelectionWriter::SelectionWriter(DiveWriter *writer)
 {
     m_writer = writer;
+
+    connect(m_writer, &DiveWriter::diveWritten, [this](Dive* d) {
+        emit diveWritten(d);
+    });
+}
+
+SelectionWriter::~SelectionWriter()
+{
+    delete m_writer;
+    m_writer = NULL;
+}
+
+void SelectionWriter::selected(QList<Dive> dives)
+{
+    m_writer->start();
 }
 
 void SelectionWriter::do_start()
@@ -12,8 +27,7 @@ void SelectionWriter::do_start()
 
 void SelectionWriter::do_end()
 {
-    delete m_writer;
-    m_writer = NULL;
+    emit select(m_dives);
 }
 
 void SelectionWriter::do_work(Dive *d)
@@ -23,15 +37,18 @@ void SelectionWriter::do_work(Dive *d)
 
 void SelectionWriter::set_device_descriptor(dc_descriptor_t *descr)
 {
-
+    m_descriptor = descr;
 }
 
 void SelectionWriter::set_device_info(uint model, uint serial, uint firmware)
 {
-
+    m_model = model;
+    m_serial = serial;
+    m_firmware = firmware;
 }
 
 void SelectionWriter::set_device_clock(uint devtime, uint systime)
 {
-
+    m_devtime = devtime;
+    m_systime = systime;
 }
