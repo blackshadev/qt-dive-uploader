ded06ddb3d2679cf3018f0043c9a621ab38c68f7
Vincent Hagen <vinnie@script4web.nl>
2020-07-05 20:15:20 +0200

    working on context

--- divecomputer/dccontext.cpp
+++ divecomputer/dccontext.cpp
@@ -1,6 +1,51 @@
 #include "dccontext.h"
 
+
 DCContext::DCContext()
 {
+    dc_context_new(&context);
+    log = NULL;
+    dc_context_set_logfunc(context, logfunction, this);
+}
 
+DCContext::~DCContext()
+{
+
+}
+
+void DCContext::setLogFunction(logfunc_t logFunc)
+{
+    log = logFunc;
+}
+
+inline dc_loglevel_t translateLogLevel(loglevel_t loglevel)
+{
+    return (dc_loglevel_t)loglevel;
+}
+
+void DCContext::setLogLevel(loglevel_t level)
+{
+    logLevel = level;
+    dc_context_set_loglevel(context, translateLogLevel(level));
+}
+
+std::vector<DCDeviceDescriptor> DCContext::getDescriptors()
+{
+
+}
+
+void DCContext::logfunction(dc_context_t *context, dc_loglevel_t loglevel, const char *file, unsigned int line, const char *function, const char *msg, void *userdata)
+{
+    auto ctx = (DCContext *)userdata;
+    if (ctx->log == NULL) {
+        return;
+    }
+
+    logdata_t logdata;
+    logdata.function = function;
+    logdata.loglevel = loglevel;
+    logdata.file = file;
+    logdata.line = line;
+    logdata.msg = msg;
+    ctx->log(logdata);
 }
--- divecomputer/dccontext.h
+++ divecomputer/dccontext.h
@@ -1,13 +1,43 @@
 #ifndef DCCONTEXT_H
 #define DCCONTEXT_H
 #include "dcdevicedescriptor.h"
+#include <libdivecomputer/context.h>
 #include <vector>
+#include <functional>
+#include <string>
+
+typedef dc_loglevel_t loglevel_t;
+typedef struct {
+    dc_loglevel_t loglevel;
+    const char *file;
+    unsigned int line;
+    const char *function;
+    const char *msg;
+} logdata_t;
+typedef std::function<void (logdata_t)> logfunc_t;
+
+class DCTransport
+{
+public:
+    enum Type {
+
+    };
+};
 
 class DCContext
 {
 public:
     DCContext();
+    ~DCContext();
+    void setLogFunction(logfunc_t logFunc);
+    void setLogLevel(loglevel_t logLevel);
     std::vector<DCDeviceDescriptor> getDescriptors();
+    std::vector<DCTransport> getTransport(DCTransport::Type flag);
+private:
+    loglevel_t logLevel = DC_LOGLEVEL_ERROR;
+    logfunc_t log;
+    dc_context_t *context;
+    static void logfunction(dc_context_t *context, dc_loglevel_t loglevel, const char *file, unsigned int line, const char *function, const char *msg, void *userdata);
 };
 
 #endif // DCCONTEXT_H
